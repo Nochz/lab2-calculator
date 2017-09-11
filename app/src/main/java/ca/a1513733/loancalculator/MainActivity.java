9cb63e1a4c9c5e66ab0fed4809b7aa5676b42507
Rhai Hinds <rhai.hinds@dawsoncollege.qc.ca>
2017-09-11 14:24:15 -0400

    Method to help display loan information added

--- app/src/main/java/ca/a1513733/loancalculator/MainActivity.java
+++ app/src/main/java/ca/a1513733/loancalculator/MainActivity.java
@@ -10,6 +10,8 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     EditText etLoanAmt, etYears, etRate;
+    TextView amt,years, yrInterest;
+    TextView tlInterest, payment;
     TextView result;
     LoanCalculator loanCalc;
 
@@ -28,7 +30,13 @@ public class MainActivity extends AppCompatActivity {
         etYears.setFilters(new InputFilter[] {new InputFilter.LengthFilter(3)});
         etRate= (EditText) findViewById(R.id.rate);
         etRate.setFilters(new InputFilter[] {new InputFilter.LengthFilter(4)});
-        result = (TextView) findViewById(R.id.res);
+        result = (TextView) findViewById(R.id.costText);
+
+        amt = (TextView) findViewById(R.id.amtText);
+        years = (TextView) findViewById(R.id.yearsText);
+        yrInterest = (TextView) findViewById(R.id.yrInterestText);
+        tlInterest = (TextView) findViewById(R.id.tlInterestText);
+        payment = (TextView) findViewById(R.id.paymentText);
     }
 
     public void calcLoan(View v) {
@@ -37,10 +45,17 @@ public class MainActivity extends AppCompatActivity {
         loanCalc.setNumberOfYears(Integer.parseInt(etYears.getText().toString()));
         loanCalc.setYearlyInterestRate(Double.parseDouble(etRate.getText().toString()));
 
+        displayLoanInfo(v);
     }
 
-    public void displayLoanInfo() {
-
+    public void displayLoanInfo(View v) {
+        result.setText(loanCalc.getTotalCostOfLoan()+"");
+        amt.setText(loanCalc.getLoanAmount()+"");
+        years.setText(loanCalc.getNumberOfYears()+"");
+        yrInterest.setText(loanCalc.getYearlyInterestRate()+"");
+        tlInterest.setText(loanCalc.getTotalInterest()+"");
+        payment.setText(loanCalc.getMonthlyPayment()+"");
+        
     }
 
 }
